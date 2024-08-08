a5a06e40efbd2f7dfa74feb8c66b719c119c74b2
Begüm Başovalı <begumbasovali@posta.mu.edu.tr>
2024-08-09 01:43:33 +0300

    My React First Project

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+      <nav className="nav">
+          <img src="./images/reactjs-icon.png" className="nav-logo"/>
+          <h3 className="nav-logo-text">ReactFacts</h3>
+          <h4 className="nav-title">React Course Project-1</h4>
+          
+        </nav>
+        
+      
+      
+    )
+}
